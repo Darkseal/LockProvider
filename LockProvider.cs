b384f2dce7d54d413f0695c0e038b8c873b21fa6
Dark <games@myst.it>
2021-03-19 15:06:32 +0100

    Update LockProvider.cs
    Updated Release method with inner logic to keep track of waiters

--- LockProvider.cs
+++ LockProvider.cs
@@ -7,11 +7,14 @@ namespace Ryadel.Components.Threading
     /// <summary>
     /// A LockProvider based upon the SemaphoreSlim class to selectively lock objects, resources or statement blocks 
     /// according to given unique IDs in a sync or async way.
+    /// 
+    /// SAMPLE USAGE & ADDITIONAL INFO:
+    /// - https://www.ryadel.com/en/asp-net-core-lock-threads-async-custom-ids-lockprovider/
+    /// - https://github.com/Darkseal/LockProvider/
     /// </summary>
     public class LockProvider<T>
     {
-        static readonly ConcurrentDictionary<T, SemaphoreSlim> lockDictionary = new ConcurrentDictionary<T, SemaphoreSlim>();
-
+        static readonly ConcurrentDictionary<T, InnerSemaphore> lockDictionary = new ConcurrentDictionary<T, InnerSemaphore>();
 
         public LockProvider() { }
 
@@ -21,7 +24,7 @@ namespace Ryadel.Components.Threading
         /// <param name="idToLock">the unique ID to perform the lock</param>
         public void Wait(T idToLock)
         {
-            lockDictionary.GetOrAdd(idToLock, new SemaphoreSlim(1, 1)).Wait();
+            lockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1)).Wait();
         }
 
         /// <summary>
@@ -30,20 +33,46 @@ namespace Ryadel.Components.Threading
         /// <param name="idToLock">the unique ID to perform the lock</param>
         public async Task WaitAsync(T idToLock)
         {
-            await lockDictionary.GetOrAdd(idToLock, new SemaphoreSlim(1, 1)).WaitAsync();
+            await lockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1)).WaitAsync();
         }
 
-        /// <summary>
-        /// Releases the lock (according to the given ID)
-        /// </summary>
-        /// <param name="idToUnlock">the unique ID to unlock</param>
         public void Release(T idToUnlock)
         {
-            SemaphoreSlim semaphore;
-            if (lockDictionary.TryRemove(idToUnlock, out semaphore))
-            {
+            InnerSemaphore semaphore;
+            if (lockDictionary.TryGetValue(idToUnlock, out semaphore))
                 semaphore.Release();
-            }
+        }
+    }
+
+    public class InnerSemaphore
+    {
+        private SemaphoreSlim _semaphore;
+        private int _waiters;
+
+        public InnerSemaphore(int initialCount, int maxCount)
+        {
+            _semaphore = new SemaphoreSlim(initialCount, maxCount);
+            _waiters = 0;
+        }
+
+        public void Wait()
+        {
+            _waiters++;
+            _semaphore.Wait();
+        }
+
+        public async Task WaitAsync()
+        {
+            _waiters++;
+            await _semaphore.WaitAsync();
+        }
+
+        public void Release()
+        {
+            _waiters--;
+            _semaphore.Release();
+            if (_waiters == 0)
+                _semaphore.Dispose();
         }
     }
 }
